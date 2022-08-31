30074f60da1bcb3e9d22efd2f7e182b49d5a0935
Vjworld <vaibhav.selukar@gmail.com>
2022-08-31 18:48:47 +0530

    First commit Add prog

---
+++ addd.py
@@ -0,0 +1,9 @@
+#name = input("What is your Name? ")
+#print("You provided your name as Mr. "+name+"") 
+
+num1 = int(input("please provide 1st number to add: "))
+num2 = int(input("please provide 2nd number to add: "))
+print("You provided two numbers to add-> "+num1+" and "+num2+" ")
+
+print("total of numbers you provided = "+ (sum: num1+num2) +)
+
